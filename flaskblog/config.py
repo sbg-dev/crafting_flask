6351959e60f536d0f07ee9f482eff2e6c0b1406b
Sebastian Gnip <travin42@gmail.com>
2020-07-29 19:46:13 +0200

    Fix the config so that that PW reset now works.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,7 @@ import os
 
 class config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = os.environ.get('smtp.gmail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
